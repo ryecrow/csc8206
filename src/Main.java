a78c88ced25182c93f017b5bec7ec6aeb8170bad
tommyJin <tommyjing0620@gmail.com>
2016-03-08 01:41:42 +0000

    a little change for test

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
+        List<String> input = new ArrayList<>();
+        List<String> output = new ArrayList<>();
+
+
+
+
+        input.add("s1;s7");
+        input.add("s1;s7");
+        input.add("s8;s2");
+
         Track track = new Track();
         track.read();
 
@@ -31,37 +41,50 @@ public class Main {
         f.setSize(new Dimension(1500, 1000));
         f.setVisible(true);
 
+        for (int i = 0; i < input.size(); i++) {
+            //invoke Richard method
+            String[] s = input.get(i).split(";");
+            String source = s[0];
+            String dest = s[1];
+            String rs =  track.findRoute(source, dest);
+            System.out.println("source="+source+" ; dest="+dest+" ; rs="+rs);
+            output.add(rs);
+        }
 
         LinkedList<String> signalsforEachRoute = new LinkedList<String>();
-        signalsforEachRoute = track.upPath("s1", "s11");
+//        signalsforEachRoute = track.findRoute("s1", "s7");
 
         Interlock interlock = new InterlockImpl();
-        String filepath = "./resource/map2.json";
+        String filepath = "./resource/map1.json";
         //create a railway object
         Railway railway = new Railway(filepath);
 
 
 
         //test here
-        signalsforEachRoute.clear();
-        signalsforEachRoute.add("s1;s4;s8;s11");
-        signalsforEachRoute.add("s1;s6;s8;s11");
-        signalsforEachRoute.add("s12;s9;s3;s2");
-        signalsforEachRoute.add("s12;s7;s5;s2");
+//        signalsforEachRoute.clear();
+//        signalsforEachRoute.add("s1;s4;s7");
+//        signalsforEachRoute.add("s1;s4;s7");
+//        signalsforEachRoute.add("s1;s4;s8;s11");
+//        signalsforEachRoute.add("s1;s6;s8;s11");
+//        signalsforEachRoute.add("s12;s9;s3;s2");
+//        signalsforEachRoute.add("s12;s7;s5;s2");
 
 
 
 
-        System.out.println("Richard result="+signalsforEachRoute.size());
-        for(int z = 0; z < signalsforEachRoute.size(); z++){
-            System.out.println(signalsforEachRoute.get(z));
+//        System.out.println("Richard result="+signalsforEachRoute.size());
+
+        //loop output list
+        for(int z = 0; z < output.size(); z++){
+            System.out.println(" 111  "+output.get(z));
 
 
 
             //check interface demo
             Map<String,Object> map = new HashMap<>();
 
-            String journey = signalsforEachRoute.get(z).substring( signalsforEachRoute.get(z).length()-2 , signalsforEachRoute.get(z).length() - 1).equals(";")?signalsforEachRoute.get(z).substring(0, signalsforEachRoute.get(z).length() - 1):signalsforEachRoute.get(z);
+            String journey = output.get(z).substring( output.get(z).length()-2 , output.get(z).length() - 1).equals(";")?output.get(z).substring(0, output.get(z).length() - 1):output.get(z);
 
             map.put("journey",journey);
             map.put("journeyId","j"+(z+1));
