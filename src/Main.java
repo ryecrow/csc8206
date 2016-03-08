b4847595ee7dd05a85124b768398a3f967460f40
tommyJin <tommyjing0620@gmail.com>
2016-03-08 14:59:54 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import Richard.Track;
-import Yiwei.Interlock;
-import Yiwei.InterlockImpl;
-import Yiwei.Railway;
-import Yiwei.Route;
+import yiwei.Interlock;
+import yiwei.InterlockImpl;
+import yiwei.Railway;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,19 +13,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-
+        String name = "map2";
+        String filepath = "./resource/"+name;
         List<String> input = new ArrayList<>();
         List<String> output = new ArrayList<>();
 
 
-
-
-        input.add("s1;s7");
-        input.add("s1;s7");
-        input.add("s8;s2");
+        input.add("s1;s11");
+//        input.add("s1;s11");
+//        input.add("s1;s11");
+//        input.add("s1;s11");
+//        input.add("s12;s2");
+//        input.add("s12;s2");
+        input.add("s12;s2");
+        input.add("s12;s2");
 
         Track track = new Track();
-        track.read();
+        track.read(filepath + ".txt");
 
         JFrame f = new JFrame("Train Track Demo");
         f.addWindowListener(new WindowAdapter() {
@@ -39,64 +42,42 @@ public class Main {
         applet.init();
         f.pack();
         f.setSize(new Dimension(1500, 1000));
-        f.setVisible(true);
+        f.setVisible(false);
+
 
         for (int i = 0; i < input.size(); i++) {
             //invoke Richard method
             String[] s = input.get(i).split(";");
             String source = s[0];
             String dest = s[1];
-            String rs =  track.findRoute(source, dest);
-            System.out.println("source="+source+" ; dest="+dest+" ; rs="+rs);
+            String rs = track.findRoute(source, dest);
+            System.out.println("source=" + source + " ; dest=" + dest + " ; rs=" + rs);
             output.add(rs);
         }
 
+
         LinkedList<String> signalsforEachRoute = new LinkedList<String>();
-//        signalsforEachRoute = track.findRoute("s1", "s7");
 
         Interlock interlock = new InterlockImpl();
-        String filepath = "./resource/map1.json";
         //create a railway object
-        Railway railway = new Railway(filepath);
+        Railway railway = new Railway(filepath + ".json");
 
-
-
-        //test here
-//        signalsforEachRoute.clear();
-//        signalsforEachRoute.add("s1;s4;s7");
-//        signalsforEachRoute.add("s1;s4;s7");
-//        signalsforEachRoute.add("s1;s4;s8;s11");
-//        signalsforEachRoute.add("s1;s6;s8;s11");
-//        signalsforEachRoute.add("s12;s9;s3;s2");
-//        signalsforEachRoute.add("s12;s7;s5;s2");
-
-
-
-
-//        System.out.println("Richard result="+signalsforEachRoute.size());
 
         //loop output list
-        for(int z = 0; z < output.size(); z++){
-            System.out.println(" 111  "+output.get(z));
-
-
+        for (int z = 0; z < output.size(); z++) {
 
             //check interface demo
-            Map<String,Object> map = new HashMap<>();
+            Map<String, Object> map = new HashMap<>();
 
-            String journey = output.get(z).substring( output.get(z).length()-2 , output.get(z).length() - 1).equals(";")?output.get(z).substring(0, output.get(z).length() - 1):output.get(z);
+            String journey = output.get(z).substring(output.get(z).length() - 2, output.get(z).length() - 1).equals(";") ? output.get(z).substring(0, output.get(z).length() - 1) : output.get(z);
 
-            map.put("journey",journey);
-            map.put("journeyId","j"+(z+1));
+            map.put("journey", journey);
+            map.put("journeyId", "j" + (z + 1));
 
 
             boolean flag = interlock.check(railway, map);
 
-            if (flag){
-                break;
-            }
         }
-
 
         List<Railway> railways = interlock.running(railway);
 
